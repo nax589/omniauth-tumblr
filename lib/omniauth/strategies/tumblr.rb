08811583160ba26f852f584b69a01f3b14b1ef7b
nao <naoabc@gmail.com>
2016-05-31 17:58:28 +0900

    delete oauth_everifier #_=_

--- lib/omniauth/strategies/tumblr.rb
+++ lib/omniauth/strategies/tumblr.rb
@@ -47,6 +47,41 @@ module OmniAuth
         res = access_token.get(url).body
         @avatar_url ||= MultiJson.decode(res)['response']['avatar_url']
       end
+
+      alias :old_callback_phase :callback_phase
+
+      def callback_phase # rubocop:disable MethodLength
+        fail(OmniAuth::NoSessionError, "Session Expired") if session["oauth"].nil?
+
+        request_token = ::OAuth::RequestToken.new(consumer, session["oauth"][name.to_s].delete("request_token"), session["oauth"][name.to_s].delete("request_secret"))
+
+        opts = {}
+        if session["oauth"][name.to_s]["callback_confirmed"]
+          opts[:oauth_verifier] = request["oauth_verifier"].gsub('#_=_', '')
+        else
+          opts[:oauth_callback] = callback_url
+        end
+        @access_token = request_token.get_access_token(opts)
+        # old_callback_phase
+        env['omniauth.auth'] = auth_hash
+        call_app!
+      rescue ::Timeout::Error => e
+        fail!(:timeout, e)
+      rescue ::Net::HTTPFatalError, ::OpenSSL::SSL::SSLError => e
+        fail!(:service_unavailable, e)
+      rescue ::OAuth::Unauthorized => e
+        fail!(:invalid_credentials, e)
+      rescue ::OmniAuth::NoSessionError => e
+        fail!(:session_expired, e)
+      end
+
+      credentials do
+        {"token" => access_token.token, "secret" => access_token.secret}
+      end
+
+      extra do
+        {"access_token" => access_token}
+      end
     end
   end
 end
